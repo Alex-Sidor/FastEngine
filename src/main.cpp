4edface3476c8131fa64f4f944c1b3c13202b161
AlexSid0 <astro_studios@outlook.com>
2025-07-14 08:33:07 +0100

    per vertex transformations instead of per triangle and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@
 #include <chrono>
 #include <cmath>
 
+#include <string>
+
 #include <math.h>
 
 vec3 objectVertexBuffer[5];
 
-int objectTriangleBuffer[18] = {0,1,4,0,1,2,3,1,2,2,3,4,0,2,4,3,1,4};
+int objectTriangleBuffer[18] = {0,1,4,0,1,2,3,2,1,3,2,4,0,4,2,1,3,4};
 
 vec3 objectVertex[5] = {{-1,1,2},{1,1,2},{-1,1,4},{1,1,4},{0,-1,3}};
 
@@ -26,9 +28,6 @@ public:
 
     float angle = 0;
     float fov;
-    
-    float viewportScaleX;
-    float viewportScaleY;
 
     Camera(int width, int height,float fieldOfView, float scale){
         WINDOW_WIDTH = width;
@@ -43,8 +42,8 @@ public:
         viewportScaleY = (halfHeight * scale) / fov;
 
         amountOfPixels = width * height;
-        largestSize = height;
-        bufferSize = largestSize * 2;
+        bufferOffset = height;
+        bufferSize = bufferOffset * 2;
 
         pixelBuffer = new Uint32[amountOfPixels];   
 
@@ -74,8 +73,9 @@ public:
         float c = cosf(angle);
 
         for(int i = 0; i < 5; i++){
-		objectVertexBuffer[i] = rotateVector(objectVertex[i],{0,0,3},s,c);
-	}
+		    rotateVector(objectVertex[i],objectVertexBuffer[i],{0,0,3},s,c);
+            projectVertex(objectVertexBuffer[i],objectVertexBuffer[i]);
+	    }
 
         for(int i = 0;i < 18; i+=3){
             triangle(objectVertexBuffer[objectTriangleBuffer[i]],objectVertexBuffer[objectTriangleBuffer[i+1]],objectVertexBuffer[objectTriangleBuffer[i+2]]);
@@ -97,25 +97,38 @@ private:
     int halfWidth;
     int halfHeight;
     int amountOfPixels;
-    int largestSize;
+    int bufferOffset;
     int bufferSize;
 
-    vec3 rotateVector(vec3 original,vec3 pivot,float sin,float cos){
-	float newX = original.x-pivot.x;
-	float newZ = original.z-pivot.z;
+    float viewportScaleX;
+    float viewportScaleY;
 
-        return (vec3){((newX*cos)-(newZ*sin))+pivot.x,original.y,((newX*sin)+(newZ*cos))+pivot.z};
+    void projectVertex(vec3& original,vec3& transformed){
+        transformed.x = (viewportScaleY * original.x / (original.z * fov)) + halfWidth;
+        transformed.y = (viewportScaleY * original.y / (original.z * fov)) + halfHeight;
+    }
+
+    void rotateVector(vec3& original,vec3& tramsformed,vec3 pivot,float sin,float cos){
+        float newX = original.x-pivot.x;
+        float newZ = original.z-pivot.z;
+
+        tramsformed.x = ((newX*cos)-(newZ*sin))+pivot.x;
+        tramsformed.y = original.y;
+        tramsformed.z = ((newX*sin)+(newZ*cos))+pivot.z;
     }
 
     void minMaxPlot(int x0, int y0,vec3 c0) {
-        if (x0 < 0 || x0 > WINDOW_WIDTH -1 || y0 < 0 || y0 > WINDOW_HEIGHT-1) return;
+        if (x0 < 0 || x0 > WINDOW_WIDTH -1 || y0 < 0 || y0 > WINDOW_HEIGHT-1){
+            //::cout << "pixel out of range\n";
+            return;
+        }
         
         if (triangleBuffer[y0] == -1) {
             triangleBuffer[y0] = x0;
-            triangleBuffer[y0 + largestSize] = x0;
+            triangleBuffer[y0 + bufferOffset] = x0;
             
             triangleWeights[y0] = c0;
-            triangleWeights[y0 + largestSize] = c0;
+            triangleWeights[y0 + bufferOffset] = c0;
 
         } else {
             if (x0 < triangleBuffer[y0]){
@@ -123,45 +136,45 @@ private:
                 triangleWeights[y0] = c0;
             }
             else {
-                if (x0 > triangleBuffer[y0 + largestSize]){
-                    triangleBuffer[y0 + largestSize] = x0;
-                    triangleWeights[y0 + largestSize] = c0;
+                if (x0 > triangleBuffer[y0 + bufferOffset]){
+                    triangleBuffer[y0 + bufferOffset] = x0;
+                    triangleWeights[y0 + bufferOffset] = c0;
                 }
             }   
         }
     }
 
-    inline void drawLine(vec2 p0, vec2 p1, vec3 c0, vec3 c1) {
+    inline void drawLine(vec2 p0, vec2 p1, vec3 w0, vec3 w1) {
         float dx = p1.x - p0.x;
-        float dy = p1.y - p0.y;
+        float dy = 1/(p1.y - p0.y);
 
-        float slope = dx/dy;
+        float slope = dx*dy;
 
         int start = static_cast<int>(SDL_roundf(p0.y));
         int end = static_cast<int>(SDL_roundf(p1.y));
 
-        vec3 colourSlope = ((c1 - c0)/dy);
+        vec3 colourSlope = (w1 - w0)*dy;
 
         if(start>end){
             for (int y = end; y < start; y++)
             {
-                minMaxPlot(static_cast<int>(p1.x),y,c1);
+                minMaxPlot(static_cast<int>(p1.x),y,w1);
                 p1.x += slope;
             
-                c1 += colourSlope;
+                w1 += colourSlope;
             }
         }else{
             for (int y = start; y < end; y++)
             {
-                minMaxPlot(static_cast<int>(p0.x),y,c0);
+                minMaxPlot(static_cast<int>(p0.x),y,w0);
                 p0.x += slope;
             
-                c0 += colourSlope;
+                w0 += colourSlope;
             }
         } 
     }
 
-    void triangle(vec3 p0, vec3 p1, vec3 p2) { 
+    inline void triangle(vec3 p0, vec3 p1, vec3 p2) { 
         
         if(p0.z <= 0 || p1.z <= 0 || p2.z <= 0){
             std::cout << "behind viewport - move forwards\n";
@@ -175,19 +188,11 @@ private:
         float v0 = 0;
         float v1 = 0;
         float v2 = 1;
-
-        p0.x = (viewportScaleY * p0.x / (p0.z * fov)) + halfWidth;
-        p1.x = (viewportScaleY * p1.x / (p1.z * fov)) + halfWidth;
-        p2.x = (viewportScaleY * p2.x / (p2.z * fov)) + halfWidth;
         
-        p0.y = (viewportScaleY * p0.y / (p0.z * fov)) + halfHeight;
-        p1.y = (viewportScaleY * p1.y / (p1.z * fov)) + halfHeight;
-        p2.y = (viewportScaleY * p2.y / (p2.z * fov)) + halfHeight;
-        
-        vec2 v1d = (vec2){p1.x - p0.x,p1.y - p0.y};
-        vec2 v2d = (vec2){p2.x - p0.x,p2.y - p0.y};
+        vec2 edgeVector1 = (vec2){p1.x - p0.x,p1.y - p0.y};
+        vec2 edgeVector2 = (vec2){p2.x - p0.x,p2.y - p0.y};
 
-        if(((v1d.y*v2d.x) - (v1d.x*v2d.y)) <= 0){
+        if(((edgeVector1.y*edgeVector2.x) - (edgeVector1.x*edgeVector2.y)) <= 0){
           return;
         }
         
@@ -211,11 +216,11 @@ private:
         maxY = static_cast<int>(roundf(maxY));
 
         if(minY < 0) minY = 0;
-        if(maxY > largestSize -1) maxY = largestSize;
+        if(maxY > bufferOffset -1) maxY = bufferOffset;
 
-        drawLine((vec2){p0.x, p0.y}, (vec2){p1.x, p1.y}, (vec3){1,0,0}, (vec3){0,1,0});
-        drawLine((vec2){p1.x, p1.y}, (vec2){p2.x, p2.y}, (vec3){0,1,0}, (vec3){0,0,1});
-        drawLine((vec2){p2.x, p2.y}, (vec2){p0.x, p0.y}, (vec3){0,0,1}, (vec3){1,0,0});
+        drawLine({p0.x, p0.y}, {p1.x, p1.y}, {1,0,0}, {0,1,0});
+        drawLine({p1.x, p1.y}, {p2.x, p2.y}, {0,1,0}, {0,0,1});
+        drawLine({p2.x, p2.y}, {p0.x, p0.y}, {0,0,1}, {1,0,0});
         
         float pixelZ;
         
@@ -234,16 +239,16 @@ private:
         float invp2z = 1/p2.z;
         
         for (int y = minY; y < maxY+1; y++) {          
-            vec3 currentColour = triangleWeights[y];
+            vec3 currentColour = triangleWeights[y];/* +1 i have no idea what this did before but when i removed it it fixed weird artifacts*/
             
-            vec3 slope = (triangleWeights[y + largestSize] - triangleWeights[y]) / (triangleBuffer[y + largestSize] - triangleBuffer[y]);
+            vec3 slope = (triangleWeights[y + bufferOffset] - triangleWeights[y]) / (triangleBuffer[y + bufferOffset] - triangleBuffer[y]);
             
-            for (int x = triangleBuffer[y]; x < triangleBuffer[y + largestSize]/* +1 i have no idea what this did before but when i removed it it fixed weird artifacts*/; x++) {
+            for (int x = std::max(triangleBuffer[y],0) + p; x < std::min(triangleBuffer[y + bufferOffset],WINDOW_WIDTH-1) + p; x++) {
                 
                 pixelZ = 1/((triangleWeights[y].x * invp0z) + (triangleWeights[y].y * invp1z) + (triangleWeights[y].z * invp2z));
 
-                if (pixelDepthBuffer[p + x] > pixelZ || pixelDepthBuffer[p + x] == 0){
-                    pixelDepthBuffer[p + x] = pixelZ;
+                if (pixelDepthBuffer[x] > pixelZ || pixelDepthBuffer[x] == 0){
+                    pixelDepthBuffer[x] = pixelZ;
                     
                     float U = ((triangleWeights[y].x * u0invp0z) + (triangleWeights[y].y * u1invp1z) + (triangleWeights[y].z * u2invp2z)) * pixelZ;
                     float V = ((triangleWeights[y].x * v0invp0z) + (triangleWeights[y].y * v1invp1z) + (triangleWeights[y].z * v2invp2z)) * pixelZ;
@@ -251,9 +256,9 @@ private:
                     //shader start
                     
                     if(((static_cast<int>(V*10)-static_cast<int>(U*10)) & 1) == 0){
-                        pixelBuffer[p + x] = 4294967295; //white (2^32)-1
+                        pixelBuffer[x] = 4294967295; //white (2^32)-1
                     }else{
-                        pixelBuffer[p + x] = 255; //black (2^8)-1
+                        pixelBuffer[x] = 255; //black (2^8)-1
                     }
                     
                     //shader end
@@ -263,11 +268,21 @@ private:
             }
             
             triangleBuffer[y] = -1;
-            triangleBuffer[y+largestSize] = -1;
+            triangleBuffer[y+bufferOffset] = -1;
             p += WINDOW_WIDTH;
         }
     }
 };
+
+void count(int*array,int size){
+    for(int i = 0; i< size; i++){
+        *array=i;
+        array ++;
+    }
+}
+
+float avgFps = 0;
+Uint8 frameCount = 0;
 
 int main(int argc, char* argv[]) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -276,19 +291,27 @@ int main(int argc, char* argv[]) {
     }
 
     Screen screen(1000, 1000);
-    Camera camera(1000, 1000,120,0.5);
+    Camera camera(1000, 1000,120,1);
 
     std::cout << screen.error << "\n";
 
     bool running = true;
     SDL_Event event;
 
+    int array[100];
+
+    count(array, sizeof(array)/sizeof(int));
+    
+    std::cout << array[69] << "\n";
+
+    int a;
+    std::cout << a << "\n";
+
     while(running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT)
                 running = false;
         }
-
         auto start = std::chrono::high_resolution_clock::now();
 
         camera.renderBuffer();
@@ -296,11 +319,17 @@ int main(int argc, char* argv[]) {
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         
-        SDL_Log("%ld frames per second", 1000000/duration.count());
+        avgFps = (avgFps * 0.99) + (10000/duration.count());
         
         //camera.convertDepthIntoGrayscaleAndDisplayTobuffer(0,10);
 
         screen.displayBuffer(camera.pixelBuffer);
+
+        if((frameCount % 1000) == 0){
+            SDL_Log("f:%d", static_cast<int>(avgFps));
+        }
+
+        frameCount ++;
     }
     SDL_Quit();
 }
