ba2084eabcc90c7c718d83ec958d9f85409ec04a
Alex <astro_studios@outlook.com>
2026-01-01 18:03:56 +0000

    seperated fps measuring

---
+++ FastEngineCore/include/FrameTime.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <chrono>
+
+namespace Helpers {
+	float getFrameRate() {
+        static auto last;
+        auto current = std::chrono::high_resolution_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(current - last);
+
+        last = current;
+
+        return 1000000.0f / static_cast<float>(duration.count());
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 #include <algorithm>
-#include <chrono>
+
 #include <cmath>
 #include <string>
 #include <math.h>
@@ -13,9 +13,7 @@
 #include "vector.h"
 #include "Camera.h"
 #include "Mesh.h"
-
-float fps = 0;
-int frameCount = 1;
+#include "FrameTime.h"
 
 int main(int argc, char* argv[]) {
 
@@ -28,24 +26,12 @@ int main(int argc, char* argv[]) {
         if (glfwGetKey(camera.getWindow(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(camera.getWindow(), GLFW_TRUE);
         }
-        
-        auto start = std::chrono::high_resolution_clock::now();
 
         camera.clearBuffers();
 
         camera.renderMesh(triangle);
-
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        
-        fps = 1000000.0f / static_cast<float>(duration.count());
-
         camera.displayBuffer();
 
-        if((frameCount % 10) == 0){
-            std::cout << fps << "\n";
-        }
-
-        frameCount++;
+        std::cout << Helpers::getFrameRate() << "\n";
     }
 }
