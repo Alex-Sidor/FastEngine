f8d88a6c2bc3f95cbd801cfb16e39720238de046
Alex <alex_sidor@outlook.com>
2026-02-10 13:41:24 +0100

    fixed class again

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,12 +8,12 @@ Player::Player(Camera* sceneCamera)
 void Player::update(Screen* targetScreen)
 {
 	Vec3 inputVector = {
-			(float)targetScreen.inputHandler->keyPressed(GLFW_KEY_D) - (float)targetScreen.inputHandler->keyPressed(GLFW_KEY_A),
-			(float)targetScreen.inputHandler->keyPressed(GLFW_KEY_E) - (float)targetScreen.inputHandler->keyPressed(GLFW_KEY_Q),
-			(float)targetScreen.inputHandler->keyPressed(GLFW_KEY_W) - (float)targetScreen.inputHandler->keyPressed(GLFW_KEY_S),
+			(float)targetScreen->inputHandler->keyPressed(GLFW_KEY_D) - (float)targetScreen->inputHandler->keyPressed(GLFW_KEY_A),
+			(float)targetScreen->inputHandler->keyPressed(GLFW_KEY_Q) - (float)targetScreen->inputHandler->keyPressed(GLFW_KEY_E),
+			(float)targetScreen->inputHandler->keyPressed(GLFW_KEY_W) - (float)targetScreen->inputHandler->keyPressed(GLFW_KEY_S)
 	};
 
-	transform.position += inputVector * 0.01f;
+	transform.position += inputVector;
 
 
 	camera->transform.position = transform.position;
